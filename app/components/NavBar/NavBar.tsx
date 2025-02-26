7f1e75932218c137d0aaa3fa61fbf981232c9861
Dustin Stacy <dustin.stacy@outlook.com>
2025-02-26 17:28:10 -0600

    Add `connectWallet()` button to `NavBar` and update `UserSection`

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -1,17 +1,27 @@
 "use client"
 
+import { useWallet } from "providers/WalletProvider"
+
 import "./navBar.scss"
 import { BurgerMenu, Links, UserSection } from "./subcomponents"
 
 // Renders navigation Bar component that includes page links and user information
 // Renders a login button based on the value of the `login` prop
 const NavBar = () => {
+    const { isConnected, connectWallet } = useWallet()
+
     return (
         <div className='navbar between background-gradient'>
             <BurgerMenu />
             <h3 className='navbar__logo'>Food Fight</h3>
             <Links menu='navbar' />
-            <UserSection />
+            {isConnected ? (
+                <UserSection />
+            ) : (
+                <button className='navbar__connect' onClick={connectWallet}>
+                    Connect
+                </button>
+            )}
             <hr className='primary-border' />
         </div>
     )
--- app/components/NavBar/subcomponents/UserSection/UserSection.tsx
+++ app/components/NavBar/subcomponents/UserSection/UserSection.tsx
@@ -1,11 +1,17 @@
+import { useWallet } from "providers/WalletProvider"
+
 import "./userSection.scss"
 
 // This component acts as the parent component for all User-related navigation bar components
 const UserSection = () => {
+    const { accounts } = useWallet()
+
     return (
         <div className='user-section end'>
-            <hr />
-            <div className='user-info center-column'></div>
+            <p>
+                Connected Account:{" "}
+                {accounts.map((account) => `${account.slice(0, 7)}...${account.slice(-4)}`).join(", ")}
+            </p>
         </div>
     )
 }
