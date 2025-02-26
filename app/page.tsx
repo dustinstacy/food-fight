9587d54fcd6cc2b44e47a33d65e8f7d5cb06debf
Dustin Stacy <dustin.stacy@outlook.com>
2025-02-26 14:25:22 -0600

    add Home page

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import "./styles/home.scss"
+
+export default function Home() {
+    return (
+        <div className='home page start'>
+            <div className='home-wrapper'>Home</div>
+        </div>
+    )
+}
