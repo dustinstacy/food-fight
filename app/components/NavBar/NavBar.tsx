01353061d69dd4551fd7c6e60eaf5e00482cad43
Dustin Stacy <dustin.stacy@outlook.com>
2025-02-26 15:15:48 -0600

    add `Links` subcomponent

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -1,12 +1,15 @@
 "use client"
 
 import "./navBar.scss"
+import { Links, UserSection } from "./subcomponents"
 
 // Renders navigation Bar component that includes page links and user information
 // Renders a login button based on the value of the `login` prop
 const NavBar = () => {
     return (
         <div className='navbar between background-gradient'>
+            <Links menu='navbar' />
+            <UserSection />
             <hr className='gold-border' />
         </div>
     )
