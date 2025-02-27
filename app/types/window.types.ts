cd351cc2ad20074a6ead4cdb31e3ac9166c68d89
Dustin Stacy <dustin.stacy@outlook.com>
2025-02-27 13:20:44 -0600

    add ethers package and utilize proper EIP1193Provider type

--- app/types/window.types.ts
+++ app/types/window.types.ts
@@ -1,11 +1,6 @@
-declare global {
-    interface Eip1193Provider {
-        isMetaMask?: boolean
-        request: (args: { method: string; params?: any[] }) => Promise<any>
-        on: (eventName: string, callback: (...args: any[]) => void) => void
-        removeListener: (eventName: string, callback: (...args: any[]) => void) => void
-    }
+import { Eip1193Provider } from "ethers"
 
+declare global {
     interface Window {
         ethereum?: Eip1193Provider
     }
