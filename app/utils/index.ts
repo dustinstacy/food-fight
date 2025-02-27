cf0bd1680c20ff50d811758d72b7d226803d400f
Dustin Stacy <dustin.stacy@outlook.com>
2025-02-26 19:11:54 -0600

    update utils structure

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,5 +1,2 @@
-// Combines multiple class names into a single string
-// and filters out any falsy values.
-export const classSet = (...classes: Array<string>) => {
-    return classes.filter(Boolean).join(" ")
-}
+export * from "./classSet"
+export { default as customFetch } from "./customFetch"
