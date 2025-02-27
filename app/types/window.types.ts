3528e914a05abcf927d1668c007faf97875db06b
Dustin Stacy <dustin.stacy@outlook.com>
2025-02-27 15:44:17 -0600

    add extensions to `Eip1193Provider` type

--- app/types/window.types.ts
+++ app/types/window.types.ts
@@ -2,7 +2,10 @@ import { Eip1193Provider } from "ethers"
 
 declare global {
     interface Window {
-        ethereum?: Eip1193Provider
+        ethereum?: Eip1193Provider & {
+            on: (event: "accountsChanged" | "chainChanged", handler: (accounts: string[]) => void) => void
+            removeListener: (event: "accountsChanged" | "chainChanged", handler: (accounts: string[]) => void) => void
+        }
     }
 }
 
