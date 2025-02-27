eea0432a6b3a5510d3bf78dc0de06027ba4da824
Dustin Stacy <dustin.stacy@outlook.com>
2025-02-27 15:35:51 -0600

    update api fetch

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,8 +1,8 @@
 import { User } from "types"
 import { customFetch } from "utils"
 
-export const updateUser = async <T extends keyof User>(property: T, value: string) => {
-    await customFetch(`/api/profiles/${property}`, {
+export const updateUser = async <T extends keyof User>(property: T, value: string, address: string) => {
+    await customFetch(`/api/users/${address}/${property}`, {
         method: "PUT",
         body: JSON.stringify({
             [property]: value,
