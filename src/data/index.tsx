50de9c49fa7b8730acc9d1a6f292d1bd20915652
Dustin Stacy <dustin.stacy@outlook.com>
2025-04-01 15:56:11 -0500

    update `navlinks`

---
+++ src/data/index.tsx
@@ -0,0 +1,33 @@
+import { FaHome } from "react-icons/fa"
+import { IoStorefrontSharp } from "react-icons/io5"
+import { RiAuctionLine, RiTokenSwapLine } from "react-icons/ri"
+import { GiTakeMyMoney } from "react-icons/gi"
+
+// An arary of page objects used to generate navigation links.
+export const navLinks = [
+    {
+        name: "Home",
+        path: "/",
+        image: <FaHome />,
+    },
+    {
+        name: "Shop",
+        path: "/shop",
+        image: <IoStorefrontSharp />,
+    },
+    {
+        name: "Trade",
+        path: "/trade",
+        image: <RiTokenSwapLine />,
+    },
+    {
+        name: "Auctions",
+        path: "/auctions",
+        image: <RiAuctionLine />,
+    },
+    {
+        name: "Rentals",
+        path: "/rentals",
+        image: <GiTakeMyMoney />,
+    },
+]
