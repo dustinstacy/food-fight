2b3f9a38c09af9b778fcefb950fabc29917d2722
Dustin Stacy <dustin.stacy@outlook.com>
2025-04-01 16:30:49 -0500

    add missing page to links

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -2,6 +2,7 @@ import { FaHome } from "react-icons/fa"
 import { IoStorefrontSharp } from "react-icons/io5"
 import { RiAuctionLine, RiTokenSwapLine } from "react-icons/ri"
 import { GiTakeMyMoney } from "react-icons/gi"
+import { MdAccountBalance } from "react-icons/md"
 
 // An arary of page objects used to generate navigation links.
 export const navLinks = [
@@ -29,5 +30,10 @@ export const navLinks = [
         name: "Rentals",
         path: "/rentals",
         image: <GiTakeMyMoney />,
+    },
+    {
+        name: "Manage",
+        path: "/manage",
+        image: <MdAccountBalance />,
     },
 ]
