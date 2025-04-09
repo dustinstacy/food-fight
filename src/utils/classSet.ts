fca74ade44b4d53cc9b0dc359369fc2929420a5c
Dustin Stacy <dustin.stacy@outlook.com>
2025-04-08 23:26:00 -0500

    update to accept undefined

--- src/utils/classSet.ts
+++ src/utils/classSet.ts
@@ -1,5 +1,5 @@
 // Combines multiple class names into a single string
 // and filters out any falsy values.
-export const classSet = (...classes: Array<string>) => {
+export const classSet = (...classes: Array<string | undefined>) => {
     return classes.filter(Boolean).join(" ")
 }
