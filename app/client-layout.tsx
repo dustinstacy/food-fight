69eb9f3f622875c9538f30f6803d907f467d617a
Dustin Stacy <dustin.stacy@outlook.com>
2025-04-09 20:26:05 -0500

    update layout comments

--- app/client-layout.tsx
+++ app/client-layout.tsx
@@ -1,18 +1,51 @@
 "use client"
 
-import { NavBar } from "components"
+import { NavBar } from "containers"
 import { useWallet } from "providers"
 import { useEffect } from "react"
 import { IconContext } from "react-icons"
 import { useUserStore } from "stores"
 
+/**
+ * ClientLayout is a layout component that wraps the main application content.
+ * It includes a navigation bar and handles user authentication state.
+ *
+ * @notice Requires `useWallet` for wallet connection status and `useUserStore` for user data.
+ *
+ * @remarks
+ * - The `NavBar` component is rendered at the top of the layout.
+ * - The `useWallet` hook provides the current wallet accounts.
+ * - The `useUserStore` hook is used to check for the user based on the wallet address.
+ * - The `IconContext` is used to provide a className for all icons within this layout.
+ * - The layout is designed to be used in a client-side context (hence the "use client" directive).
+ *
+ * @param param0 - The children prop is the content to be rendered inside the layout.
+ *
+ * @see {@link NavBar} - Component for the navigation bar.
+ * @see {@link useWallet} - Hook for wallet connection status and accounts.
+ * @see {@link useUserStore} - Hook for user data and authentication.
+ * @see {@link IconContext} - Context for providing icon properties.
+ *
+ */
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
+    ////////////////////////////////////////////////
+    /// Hooks                                    ///
+    ////////////////////////////////////////////////
+
     const { accounts } = useWallet()
     const checkForUser = useUserStore((state) => state.checkForUser)
+
+    ////////////////////////////////////////////////
+    /// Effects                                  ///
+    ////////////////////////////////////////////////
 
     useEffect(() => {
         checkForUser(accounts[0])
     }, [accounts, checkForUser])
+
+    ////////////////////////////////////////////////
+    /// Render                                   ///
+    ////////////////////////////////////////////////
 
     return (
         <div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
 import type { Metadata } from "next"
-
 import ClientLayout from "./client-layout"
-import "styles/globals.scss"
+import "styles/breakpoints.scss"
 import "styles/containers.scss"
+import "styles/globals.scss"
 import "styles/modules.scss"
 import "styles/theme.scss"
 import { WalletProvider } from "providers/WalletProvider"
+
+///////////////////////////////////////////////
+/// Metadata                                ///
+///////////////////////////////////////////////
 
 export const metadata: Metadata = {
     title: "Food Fight",
@@ -15,6 +19,20 @@ export const metadata: Metadata = {
     description: "A Web3 collectible card game",
 }
 
+///////////////////////////////////////////////
+/// Root Layout                            ///
+///////////////////////////////////////////////
+
+/**
+ * RootLayout is the main layout component for the application.
+ * It wraps the entire application in a global context provider and sets up the HTML structure.
+ *
+ * @notice Requires `WalletProvider` for managing wallet connections.
+ *
+ * @see {@link WalletProvider} - Context provider for managing wallet connections.
+ *
+ * @param param0 - The children prop is the content to be rendered inside the layout.
+ */
 export default function RootLayout({
     children,
 }: Readonly<{
