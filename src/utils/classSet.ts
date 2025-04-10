6476ffa4c0f2c316cd24ed8fb4299557ba719880
Dustin Stacy <dustin.stacy@outlook.com>
2025-04-09 19:38:03 -0500

    document `classSet`

--- src/utils/classSet.ts
+++ src/utils/classSet.ts
@@ -1,5 +1,8 @@
-// Combines multiple class names into a single string
-// and filters out any falsy values.
+/**
+ * Combines multiple class names into a single string.
+ * @param classes - Array of class names to be combined.
+ * @returns A single string containing all the class names, separated by spaces.
+ */
 export const classSet = (...classes: Array<string | undefined>) => {
     return classes.filter(Boolean).join(" ")
 }
