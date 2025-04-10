cb3c45e1598d647a04d2b979ff9902d15aaaf44f
Dustin Stacy <dustin.stacy@outlook.com>
2025-04-09 20:48:13 -0500

    update `Home` page and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,58 @@
 "use client"
 
+import Image from "next/image"
+import { largeLogo5 } from "assets"
 import { Button } from "components"
+import { navLinks } from "data"
 import { useUserStore } from "stores"
-
 import "./home.scss"
 
+///////////////////////////////////////
+// Home Page Component              ///
+///////////////////////////////////////
+
+/**
+ * This page is responsible for rendering the home page of the application.
+ * It displays the logo and a set of buttons based on the user's authentication status.
+ *
+ * @notice Requires `navLinks` for button navigation, `useUserStore` for user authentication status,
+ * `Button` component for rendering buttons, and `largeLogo5` for the logo image.
+ *
+ * @remarks
+ * - Rendering Logic:
+ *   - If the user is authenticated, it displays buttons for navigation.
+ *   - If the user is not authenticated, it does not display any buttons.
+ *
+ * @see {@link navLinks} for the list of navigation links.
+ * @see {@link useUserStore} for user authentication status.
+ * @see {@link Button} for button component.
+ */
 export default function Home() {
+    ////////////////////////////////////////////
+    /// Hooks                                ///
+    ////////////////////////////////////////////
+
     const { user } = useUserStore()
 
+    ////////////////////////////////////////////
+    /// Render                               ///
+    ////////////////////////////////////////////
+
     return (
-        <div className='home-wrapper page center'>
-            <div className='home'>
-                <div className='title'>
-                    <h1>Food</h1>
-                    <h1>Fight</h1>
+        <div className='home page bottom background-gradient'>
+            <Image className='home__logo abs-top-center' src={largeLogo5} alt='Food Fight logo' />
+            {user ? (
+                <div className='home__buttons bottom-column'>
+                    {navLinks.map(
+                        (link) =>
+                            link.name !== "Home" && (
+                                <Button key={link.name} label={link.name} path={link.path} />
+                            )
+                    )}
                 </div>
-                {user ? (
-                    <div className='buttons'>
-                        <Button className='home-button' label='Shop' type='link' path='/shop' />
-                        <Button className='home-button' label='Trade' type='link' path='/trade' />
-                        <Button
-                            className='home-button'
-                            label='Auctions'
-                            type='link'
-                            path='/auctions'
-                        />
-                        <Button
-                            className='home-button'
-                            label='Rentals'
-                            type='link'
-                            path='/rentals'
-                        />
-                        <Button className='home-button' label='Manage' type='link' path='/manage' />
-                    </div>
-                ) : (
-                    <> </>
-                )}
-            </div>
+            ) : (
+                <> </>
+            )}
         </div>
     )
 }
