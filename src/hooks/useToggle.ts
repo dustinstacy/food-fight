51d2727459c9e7718cffb9d619dee453be750c33
Dustin Stacy <dustin.stacy@outlook.com>
2025-04-10 20:30:05 -0500

    update `useToggle` hook JSDoc

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,30 +1,31 @@
-"use-client"
+'use client'
+
+import { useState } from 'react'
+
+import { ToggleState } from 'types'
+
 /**
  * Custom Hook: useToggle
- *
  * A reusable hook that manages a boolean toggle state.
  *
  * @param {boolean} initialState - The initial state of the toggle (default: false).
- * @returns {Array} An (tuple) array containing the toggle state, toggle function, and a manual toggle state setter.
+ * @returns {ToggleState} An (tuple) array containing the toggle state, toggle function, and a manual toggle state setter.
  *
- * Usage:
+ * @example // Basic usage
  *    const [isOpen, toggleIsOpen, setToggleIsOpen] = useToggle();
  *    - isToggled: The current state of the toggle (true/false).
  *    - toggle: A function to toggle the state between true and false.
  *    - setToggleIsOpen: Set state of isOpen to desired value
  */
-import { useState } from "react"
-
-type ToggleState = [boolean, () => void, (state: boolean) => void]
 
 const useToggle = (initialState: boolean = false): ToggleState => {
-    const [toggleState, setToggleState] = useState(initialState)
+  const [toggleState, setToggleState] = useState(initialState)
 
-    const toggle = () => {
-        setToggleState((prevState) => !prevState)
-    }
+  const toggle = () => {
+    setToggleState((prevState) => !prevState)
+  }
 
-    return [toggleState, toggle, setToggleState]
+  return [toggleState, toggle, setToggleState]
 }
 
 export default useToggle
