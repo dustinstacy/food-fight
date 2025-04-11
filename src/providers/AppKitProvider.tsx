099fc85850259aa7d866ed91e8e785f16b1cece1
Dustin Stacy <dustin.stacy@outlook.com>
2025-04-10 20:29:27 -0500

    remove `WalletProvider` for `AppKitProvider`

---
+++ src/providers/AppKitProvider.tsx
@@ -0,0 +1,90 @@
+'use client'
+
+import {
+  arbitrum,
+  arbitrumSepolia,
+  base,
+  baseSepolia,
+  mainnet,
+  sepolia,
+  optimism,
+  optimismSepolia,
+  polygon,
+  polygonAmoy,
+  polygonZkEvm,
+  polygonZkEvmCardona,
+  zksync,
+  zksyncSepoliaTestnet,
+} from '@reown/appkit/networks'
+import { createAppKit } from '@reown/appkit/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import React, { type ReactNode } from 'react'
+import { cookieToInitialState, WagmiProvider, type Config } from 'wagmi'
+
+import { wagmiAdapter, projectId, appkitConfig } from 'config'
+
+////////////////////////////////////////////////
+/// Query Client                             ///
+////////////////////////////////////////////////
+
+const queryClient = new QueryClient()
+
+////////////////////////////////////////////////
+/// Metadata                                 ///
+////////////////////////////////////////////////
+
+const metadata = {
+  name: 'Food Fight',
+  description: 'A Web3 collectible card game',
+  url: typeof window !== 'undefined' ? window.location.origin : 'http://localhost:3000', // origin must match your domain & subdomain
+  icons: ['/icon.ico'],
+}
+
+////////////////////////////////////////////////
+/// Modal                                    ///
+////////////////////////////////////////////////
+
+const _modal = createAppKit({
+  adapters: [wagmiAdapter],
+  projectId: projectId!,
+  networks: [
+    arbitrum,
+    arbitrumSepolia,
+    base,
+    baseSepolia,
+    mainnet,
+    optimism,
+    optimismSepolia,
+    polygon,
+    polygonAmoy,
+    polygonZkEvm,
+    polygonZkEvmCardona,
+    zksync,
+    zksyncSepoliaTestnet,
+    ...(process.env.NODE_ENV === 'development' ? [sepolia] : []),
+  ],
+  defaultNetwork: process.env.NODE_ENV === 'development' ? sepolia : mainnet,
+  metadata: metadata,
+  features: {
+    analytics: true, // Optional - defaults to your Cloud configuration
+  },
+})
+
+////////////////////////////////////////////////
+/// Provider                                 ///
+////////////////////////////////////////////////
+
+function AppKitProvider({ children, cookies }: { children: ReactNode; cookies: string | null }) {
+  // Get initial state for Wagmi SSR hydration from cookies
+  const initialState = cookieToInitialState(appkitConfig as Config, cookies) // Use config from adapter
+
+  return (
+    // Provide Wagmi state
+    <WagmiProvider config={appkitConfig as Config} initialState={initialState}>
+      {/* Provide React Query state */}
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  )
+}
+
+export default AppKitProvider
