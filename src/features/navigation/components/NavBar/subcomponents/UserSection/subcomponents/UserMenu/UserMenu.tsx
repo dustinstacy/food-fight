64f9a23b1dfea7a8a744d68f31ff152cdb1c237c
Dustin Stacy <dustin.stacy@outlook.com>
2025-05-01 17:49:52 -0500

    update `UserMenu` accessibility and add `handleClickOutside()` event

--- src/features/navigation/components/NavBar/subcomponents/UserSection/subcomponents/UserMenu/UserMenu.tsx
+++ src/features/navigation/components/NavBar/subcomponents/UserSection/subcomponents/UserMenu/UserMenu.tsx
@@ -3,12 +3,12 @@
 import Switch from '@mui/material/Switch'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
+import { useEffect, useRef } from 'react'
 import { useDisconnect } from 'wagmi'
 
 import { UseToggleProps } from 'hooks/types'
 import { useThemeStore } from 'stores'
 import './userMenu.scss'
-import { useRef } from 'react'
 
 /**
  * Renders the user menu.
@@ -45,16 +45,48 @@ const UserMenu = ({ toggleState: isOpen, toggle: toggleIsOpen }: UseToggleProps)
   const handleDivKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
     if (event.key === 'Enter' || event.key === ' ') {
       event.preventDefault()
-      toggleTheme()
+      const role = event.currentTarget.getAttribute('data-role') // Add data-role attribute below
+      if (role === 'logout') {
+        handleLogout()
+      } else if (role === 'theme-toggle') {
+        toggleTheme()
+      }
+    }
+
+    if (event.key === 'Escape') {
+      toggleIsOpen?.()
     }
   }
+
+  useEffect(() => {
+    if (!isOpen) {
+      return
+    }
+
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        toggleIsOpen?.()
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [isOpen, toggleIsOpen])
 
   return (
     <>
       {isOpen && (
-        <div className='user-menu left-column black-border primary-gradient tilt-warp'>
+        <div ref={menuRef} className='user-menu left-column black-border primary-gradient tilt-warp' role='menu'>
           {/* ----- Account Link ----- */}
-          <Link className='user-menu__link text-shadow' href='/account' onClick={() => toggleIsOpen?.()}>
+          <Link
+            className='user-menu__link text-shadow'
+            href='/account'
+            onClick={() => toggleIsOpen?.()}
+            role='menuitem'
+          >
             <span>Account</span>
           </Link>
 
@@ -62,7 +94,8 @@ const UserMenu = ({ toggleState: isOpen, toggle: toggleIsOpen }: UseToggleProps)
           <div
             className='user-menu__toggle left'
             onClick={toggleTheme}
-            role='button'
+            role='menuitem'
+            data-role='theme-toggle'
             tabIndex={0}
             onKeyDown={handleDivKeyDown}
           >
@@ -85,6 +118,7 @@ const UserMenu = ({ toggleState: isOpen, toggle: toggleIsOpen }: UseToggleProps)
             role='button'
             tabIndex={0}
             onKeyDown={handleDivKeyDown}
+            data-role='logout'
           >
             <span className='user-menu__link'>Logout</span>
           </div>
