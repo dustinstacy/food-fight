be1d40675603361d410afaba02b0b3776baf1533
Dustin Stacy <dustin.stacy@outlook.com>
2025-05-08 21:52:26 -0500

    update packages and add wagmi config

---
+++ wagmi.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from '@wagmi/cli'
+import { react } from '@wagmi/cli/plugins'
+import { Abi } from 'viem'
+
+import { deployedContractsData } from 'data'
+
+const contractsToGenerate = []
+
+for (const [chainId, contracts] of Object.entries(deployedContractsData)) {
+  for (const [contractName, contract] of Object.entries(contracts)) {
+    if (contract.abi && contract.address) {
+      contractsToGenerate.push({
+        name: contractName,
+        abi: contract.abi as Abi,
+        address: contract.address,
+        chainId: Number(chainId),
+      })
+    }
+  }
+}
+
+export default defineConfig({
+  out: 'src/hooks/useGenerated.ts',
+  contracts: contractsToGenerate,
+  plugins: [react()],
+})
